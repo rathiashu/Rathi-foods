a0a1c09ff3c63b19a54077e0ec887703ee18dc65
Ashutosh Rathi <ashutosh.rathi@komprise.com>
2024-03-16 18:09:35 +0530

    Added custom launching message

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,26 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Rathi Foods</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <img
+          src="/logo.svg"
+          alt="Rathi Foods"
+          style={{ height: "6em", margin: "20px" }}
+        />
+        {/* <Header title="Welcome to RATHI FOODS!" /> */}
+        <p className="description">LAUNCHING SOON!!</p>
       </main>
 
-      <Footer />
+      {/* <Footer /> */}
     </div>
-  )
+  );
 }
